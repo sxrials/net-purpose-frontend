bb72e3e6bd450518a75ccc49e898917847b55adf
? <?>
2020-11-01 17:11:14 +0000

    Add empty dashboard and 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import "./App.css";
 import { Container } from "semantic-ui-react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import { Dashboard } from "./pages/Dashboard";
+import { NotFound } from "./pages/NotFound";
 
 const App = () => (
   <div className="App">
     <Container style={{ padding: "5rem 0rem" }}>
       <h1>Your Holdings</h1>
+      <Switch>
+        <Route path="/" component={Dashboard} />
+        <Route component={NotFound} />
+      </Switch>
     </Container>
   </div>
 );
