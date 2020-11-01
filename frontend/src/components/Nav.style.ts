961bb3b80db8edb3c4327f8a6a0ec1e9e36b7771
? <?>
2020-11-01 17:28:30 +0000

    Add styled-components and style active nav link

---
+++ frontend/src/components/Nav.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const NavWrapper = styled.div`
+  .active {
+    font-weight: bold;
+  }
+`;
--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Menu } from "semantic-ui-react";
 import { NavLink } from "react-router-dom";
+import { NavWrapper } from "./Nav.style";
 
 export const Nav = () => (
-  <div data-testid="nav">
+  <NavWrapper data-testid="nav">
     <Menu>
       <Menu.Item>
         <NavLink to="/" exact>
@@ -14,5 +15,5 @@ export const Nav = () => (
         <NavLink to="/add">Add holding</NavLink>
       </Menu.Item>
     </Menu>
-  </div>
+  </NavWrapper>
 );
